c9ff3b9b6f781cb56c82647d993c514c4e7a4a03
Globlobglob <artyom14752@gmail.com>
2023-01-29 18:03:51 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int count = 0;
+            for (int i = 10; i <= 98; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                    count++;
+                }
+            }
+            Console.WriteLine($" summ: {count}");*/
+
+            /*
+            int count = 0;
+            for (int i = 10; i <= 98; i++)
+            {
+                if (i % 25 == 0)
+                {
+                    Console.WriteLine(i);
+                    count++;
+                }
+            }
+            Console.WriteLine($" summ: {count}");
+            */
+            /*
+            int count = 0;
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                    count++;
+                }
+            }
+            Console.WriteLine($" summ: {count}");
+            */
+
+            /*
+            for (int x = 1; x <= 20; x += 2)
+            {
+                int y = 5 * x - 2;
+                Console.WriteLine("x = {0}, y = {1}", x, y);
+            }
+            */
+
+            /*
+            for (int x = 1; x <= 1; x += 1)
+            {
+                Console.WriteLine(x);
+            }
+            for (int x = 5; x <= 12; x += 2)
+            {
+                Console.WriteLine(x);
+            }
+            */
+        }
+    }
+}
